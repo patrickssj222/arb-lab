ba97b035594bebf22555df1015f0aae922f92767
“patrickssj222” <patrickssj222@gmail.com>
2018-11-08 13:29:01 -0500

    Added Table

--- src/components/FileDisplay.js
+++ src/components/FileDisplay.js
@@ -3,10 +3,35 @@ class FileDisplay extends Component {
   render() {
     const {data} = this.props;
     console.log("subscribed" + data);
+    if (data === undefined || data == ""){
+      return null;
+    }
+    const parsedData = JSON.parse(data)
     return (
       <div className="vcenter">
         <h2> Content </h2>
-        <textarea class="form-control" value={data}></textarea>
+        <div className="table-wrapper-scroll-y">
+          <table className="table table-bordered table-striped">
+            <thead>
+              <tr>
+                {Object.keys(parsedData[0]).map(key => (
+                    <th>{key}</th>
+                  ))
+                }
+              </tr>
+            </thead>
+            <tbody>
+              {parsedData.map(jsonObj => (
+                <tr>
+                {Object.keys(jsonObj).map(key => (
+                  <td>{jsonObj[key]}</td>
+                ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        {/*<textarea class="form-control" value={data}></textarea>*/}
       </div>
     );
   }
